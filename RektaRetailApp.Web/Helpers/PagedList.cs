522c14c38b2a2fcc4adfb936974621ca04458910
Joe Izang <joeizang@hotmail.com>
2020-10-28 02:40:33 +0100

    finished fetching a paged collection of suppliers

--- RektaRetailApp.Web/Helpers/PagedList.cs
+++ RektaRetailApp.Web/Helpers/PagedList.cs
@@ -1,18 +1,33 @@
+<<<<<<< HEAD
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+=======
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RektaRetailApp.Domain.Abstractions;
+>>>>>>> 508be61 (finished fetching a paged collection of suppliers)
 
 namespace RektaRetailApp.Web.Helpers
 {
     public class PagedList<T> : List<T> where T : class
     {
         public int MaximumPageSize { get; } = 25;
+<<<<<<< HEAD
         public int CurrentPage { get; }
 
         public int TotalPages { get; }
+=======
+        public int CurrentPage { get; private set; }
+
+        public int TotalPages { get; private set; }
+>>>>>>> 508be61 (finished fetching a paged collection of suppliers)
 
         private int _pageSize = 10;
         public int PageSize
@@ -38,6 +53,7 @@ namespace RektaRetailApp.Web.Helpers
             AddRange(items);
         }
 
+<<<<<<< HEAD
         public PagedList()
         {
             
@@ -47,6 +63,12 @@ namespace RektaRetailApp.Web.Helpers
         {
             var count = await source.CountAsync(token).ConfigureAwait(false);
             var items = await source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync(token).ConfigureAwait(false);
+=======
+        public static async Task<PagedList<T>> CreatePagedList(IQueryable<T> source, int pageNumber, int pageSize)
+        {
+            var count = await source.CountAsync().ConfigureAwait(false);
+            var items = await source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync().ConfigureAwait(false);
+>>>>>>> 508be61 (finished fetching a paged collection of suppliers)
             return new PagedList<T>(count,pageSize,pageNumber,items);
         }
     }
