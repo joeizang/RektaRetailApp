472640cda1f3d6ee4cd05dba0ae2a19b19082cc5
Joseph Izang <joeizang@hotmail.com>
2020-12-14 01:57:21 +0100

    fleshed out the sale repository

--- RektaRetailApp.Web/Services/SalesRepository.cs
+++ RektaRetailApp.Web/Services/SalesRepository.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using AutoMapper;
 using JetBrains.Annotations;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using RektaRetailApp.Domain.DomainModels;
 using RektaRetailApp.Web.Abstractions;
 using RektaRetailApp.Web.Abstractions.Entities;
@@ -16,40 +18,120 @@ using RektaRetailApp.Web.Queries.Sales;
 
 namespace RektaRetailApp.Web.Services
 {
-    public class SalesRepository : GenericBaseRepository, ISalesRepository
+        public class SalesRepository : GenericBaseRepository, ISalesRepository
     {
-        public SalesRepository([NotNull] IHttpContextAccessor accessor, [NotNull] RektaContext db) : base(accessor, db)
+        private readonly RektaContext _db;
+        private readonly IMapper _mapper;
+        private readonly DbSet<Sale> _set;
+        public SalesRepository([NotNull] IHttpContextAccessor accessor,
+            [NotNull] RektaContext db, IMapper mapper) : base(accessor, db)
         {
+            _db = db;
+            _mapper = mapper;
+            _set = _db.Sales;
         }
 
-        public Task CancelASale(CancelSaleCommand command, CancellationToken token)
+        public async Task CreateSale(CreateSaleCommand command, CancellationToken token)
         {
-            throw new NotImplementedException();
-        }
+            var products = _db.Products.Include(p => p.Inventory);
+            var sale = _mapper.Map<Sale>(command);
+            var items = _db.ItemsSold;
+            //make deductions from the quantity of every product that has been sold
+            foreach(var p in command.ProductsSold)
+            {
+                var product = await products.SingleOrDefaultAsync(x => x.Id == p.Id,token).ConfigureAwait(false);
 
-        public Task CreateSale(CreateSaleCommand command, CancellationToken token)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<PagedList<SaleApiModel>> GetAllSales(GetAllSalesQuery query, CancellationToken token)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<Sale> GetSaleById(GetSaleByIdQuery query, CancellationToken token)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task SaveAsync(CancellationToken token)
-        {
-            throw new NotImplementedException();
+                product.Quantity -= p.Quantity;
+                //if (product.Inventory != null) product.Inventory.Quantity -= p.Quantity;
+                _db.Entry(product).State = EntityState.Modified;
+                var item = new ItemSold
+                {
+                    ItemName = p.ItemName,
+                    Quantity = p.Quantity,
+                    Price = p.Price,
+                    ProductId = product.Id
+                };
+                await items.AddAsync(item, token).ConfigureAwait(false);
+            }
+            //if there are any discounts then you can calculate before persisting.
+            sale.ItemsSold.AddRange(items!);
+            await _set.AddAsync(sale, token).ConfigureAwait(false);
         }
 
         public Task UpdateSale(UpdateSaleCommand command, CancellationToken token)
         {
             throw new NotImplementedException();
         }
+
+        public Task CancelASale(CancelSaleCommand command, CancellationToken token)
+        {
+            throw new NotImplementedException();
+            //after cancellation, inventory quantities should be updated
+        }
+
+        public async Task<PagedList<SaleApiModel>> GetAllSales(GetAllSalesQuery query, CancellationToken token)
+        {  
+            var queryable = _set.AsNoTracking();
+
+            
+            //TODO: BUILD YOUR DIFFERENT QUERIES ON THE IQUERYABLE INSTANCE TO SEARCH FILTER AND ORDER
+            if (query.SearchTerm is null && query.OrderByTerm is null)
+            {
+                    var saleQueryable = queryable.Select(s => new SaleApiModel
+                    {
+                        Id = s.Id,
+                        TypeOfPayment = s.ModeOfPayment,
+                        SaleDate = s.SaleDate,
+                        SalesPerson = s.SalesPersonId,
+                        GrandTotal = s.GrandTotal,
+                        NumberOfItemsSold = s.ItemsSold.Count,
+                        TypeOfSale = s.TypeOfSale,
+                        ProductsBought = s.ItemsSold.Select(x => new ItemSoldApiModel
+                        {
+                            Id = x.Id,
+                            ItemName = x.ItemName,
+                            Price = x.Price,
+                            Quantity = x.Quantity, //this should be the quantity sold not the quantity in stock.
+                            //ProductCategory = string.Join(",", x.ProductCategories.Select(x => x.Name).ToArray())
+                        }).ToList()
+                    });
+
+                    var result = new PagedList<SaleApiModel>(
+                        await saleQueryable.CountAsync(token).ConfigureAwait(false), query.PageNumber, query.PageSize,
+                        await saleQueryable.ToListAsync(token).ConfigureAwait(false));
+                    return result;
+            }
+
+            var parsedTotal = decimal.Parse(query.SearchTerm!);
+            var parsedDate = DateTimeOffset.Parse(query.SearchTerm!);
+            queryable = queryable.Where(s => s.SalesPersonId.Equals(query.SearchTerm) || s.SaleDate.Equals(parsedDate));
+            queryable = queryable.OrderBy(x => x.SaleDate);
+            //TODO: ALSO FACTOR IN THAT YOUR RESULT MUST BE PAGINATED.
+
+            var processedResult = await PagedList<SaleApiModel>.CreatePagedList(
+                queryable.Select(x => new SaleApiModel
+                {
+                    GrandTotal = x.GrandTotal,
+                    Id = x.Id,
+                    NumberOfItemsSold = x.ItemsSold.Count,
+                    SaleDate = x.SaleDate,
+                    SalesPerson = x.SalesPersonId,
+                    TypeOfPayment = x.ModeOfPayment,
+                    TypeOfSale = x.TypeOfSale
+                }), query.PageNumber, query.PageSize, token).ConfigureAwait(false);
+            return processedResult;
+        }
+
+        public async Task<Sale> GetSaleById(GetSaleByIdQuery query, CancellationToken token)
+        {
+            var result = await _set.SingleOrDefaultAsync(x => x.Id == query.Id, token).ConfigureAwait(false);
+            return result;
+        }
+
+        public Task SaveAsync(CancellationToken token)
+        {
+            return Commit<Sale>(token);
+        }
     }
+
 }
